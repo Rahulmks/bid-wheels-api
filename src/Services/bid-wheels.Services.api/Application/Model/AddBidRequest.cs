5dadfed95de6e685dd2e32bc86b23a6d86ad3d03
rahulemis <rahulraj.m@emishealth.com>
2025-01-09 09:05:39 +0530

    feat: adding new endpoint 'addBid'

---
+++ src/Services/bid-wheels.Services.api/Application/Model/AddBidRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace bid_wheels_api.Application.Model
+{
+	public class AddBidRequest
+	{
+		[JsonPropertyName("price")]
+		public string Price { get; set; }
+
+		[JsonPropertyName("daysRequired")]
+		public string DaysRequired { get; set; }
+	}
+}
---
+++ src/Services/bid-wheels.Services.api/Controllers/DriverController.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using bid_wheels.Services.Domain;
+using bid_wheels.Services.Domain.Model;
+using bid_wheels.Services.Infrastructure;
+using bid_wheels.Services.Infrastructure.Repository;
+using bid_wheels_api.Application.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace bid_wheels_api.Controllers
+{
+	[Route("[controller]")]
+	[ApiController]
+	public class DriverController(IDriverRepository driverRepository) : ControllerBase
+	{
+
+		[HttpPost("AddBid/{orderId}/{driverId}")]
+		public async Task<IActionResult> AddBidByOrderId([FromRoute]int orderId, [FromRoute]int driverId, [FromBody] AddBidRequest addBid)
+		{
+			var Bid = new Bid()
+			{
+				Price = addBid.Price,
+				ServiceDays = addBid.DaysRequired,
+				OrderId = orderId,
+				DriverId = driverId,
+				CreatedDate = DateAndTime.Now,
+				LastModifiedDate = DateAndTime.Now
+			};
+
+			await driverRepository.AddBidAsync(Bid);
+
+			return Ok(HttpStatusCode.Created);
+		}
+	}
+}
